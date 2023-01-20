5c90bf27bfa30a55488c2efef316750d402113f9
Lucas-Ribeiro-Da-Silva <lucas.r.s2003@hotmail.com>
2023-01-20 19:45:24 -0300

    python automacao

--- pyautogui_Automacao_de_interface/BaixaRelatorioEnviaGmailTrataPlanilhaAutomatico.py
+++ pyautogui_Automacao_de_interface/BaixaRelatorioEnviaGmailTrataPlanilhaAutomatico.py
@@ -36,104 +36,115 @@ import pyautogui
 import pyperclip
 import time
 
-pyautogui.PAUSE = 1
+try:
+    pyautogui.PAUSE = 1
 
-# pyautogui.click -> clicar
-# pyautogui.press -> apertar 1 tecla
-# pyautogui.hotkey -> conjunto de teclas
-# pyautogui.write -> escreve um texto
+    # pyautogui.click -> clicar
+    # pyautogui.press -> apertar 1 tecla
+    # pyautogui.hotkey -> conjunto de teclas
+    # pyautogui.write -> escreve um texto
 
-# Passo 1: Entrar no sistema da empresa (no nosso caso é o link do drive)
-pyautogui.press('win')
-pyautogui.write('chrome')
-pyautogui.press('enter')
-#se seu google tiver a inicialização com esolha de perfil, habilite a linha a seguir
-#pyautogui.click(x=435, y=369) #altere as coordenadas para selecionar seu perfil
-pyautogui.hotkey("ctrl", "t")
-pyperclip.copy("https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga?usp=sharing")
-pyautogui.hotkey("ctrl", "v")
-pyautogui.press("enter")
+    # Passo 1: Entrar no sistema da empresa (no nosso caso é o link do drive)
+    pyautogui.press('win')
+    pyautogui.write('chrome')
+    pyautogui.press('enter')
+    #alerta de inicialização
+    pyautogui.alert("Atenção, não mexa em nada até a automação acabar!")
+    #se seu google tiver a inicialização com esolha de perfil, habilite a linha a seguir
+    #pyautogui.click(x=435, y=369) #altere as coordenadas para selecionar seu perfil
+    pyautogui.hotkey("ctrl", "t")
+    pyperclip.copy("https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga?usp=sharing")
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.press("enter")
 
-time.sleep(5)
+    time.sleep(5)
 
-# Passo 2: Navegar no sistema e encontrar a base de vendas (entrar na pasta exportar)
-pyautogui.click(x=435, y=369, clicks=2) #coloque as cordenadas referente a tela de seu computador
-time.sleep(2)
+    # Passo 2: Navegar no sistema e encontrar a base de vendas (entrar na pasta exportar)
+    pyautogui.click(x=435, y=369, clicks=2) #coloque as cordenadas referente a tela de seu computador
+    time.sleep(2)
 
-# Passo 3: Fazer o download da base de vendas
-pyautogui.click(x=435, y=369, button='right') # clicar no arquivo com botao direito
-pyautogui.click(x=684, y=928) # clicar no fazer download
-time.sleep(5) # esperar o download acabar
-#se seu computadar não pede para confirmar a permissão de salvar, desative a linha a seguir
-pyautogui.press('enter') # clicar em salvar
-time.sleep(5) # esperar o download acabar
+    # Passo 3: Fazer o download da base de vendas
+    pyautogui.click(x=435, y=369, button='right') # clicar no arquivo com botao direito
+    pyautogui.click(x=684, y=928) # clicar no fazer download
+    time.sleep(5) # esperar o download acabar
+    #se seu computadar não pede para confirmar a permissão de salvar, desative a linha a seguir
+    pyautogui.press('enter') # clicar em salvar
+    time.sleep(5) # esperar o download acabar
 
-"""
-Vamos agora ler o arquivo baixado para pegar os indicadores
-- Faturamento
-- Quantidade de Produtos
-"""
+    """
+    Vamos agora ler o arquivo baixado para pegar os indicadores
+    - Faturamento
+    - Quantidade de Produtos
+    """
 
-# Passo 4: Importar a base de vendas pro Python
-import pandas as pd
+    # Passo 4: Importar a base de vendas pro Python
+    import pandas as pd
 
-#na linha a seguir coloque a url do local de seu arquivo
-tabela = pd.read_excel(r"Caminho onde seu arquivo é salvo por padrão")
+    #na linha a seguir coloque a url do local de seu arquivo
+    tabela = pd.read_excel(r"Caminho onde seu arquivo é salvo por padrão")
 
-print(tabela)
+    print(tabela)
 
-# Passo 5: Calcular os indicadores da empresa
-faturamento = tabela["Valor Final"].sum()
-print(faturamento)
-quantidade = tabela["Quantidade"].sum()
-print(quantidade)
+    # Passo 5: Calcular os indicadores da empresa
+    faturamento = tabela["Valor Final"].sum()
+    print(faturamento)
+    quantidade = tabela["Quantidade"].sum()
+    print(quantidade)
 
-"""
-Vamos agora enviar um e-mail pelo gmail
-"""
+    """
+    Vamos agora enviar um e-mail pelo gmail
+    """
 
-# Passo 6: Enviar um e-mail para a diretoria com os indicadores de venda
+    # Passo 6: Enviar um e-mail para a diretoria com os indicadores de venda
 
-# abrir aba
-pyautogui.hotkey("ctrl", "t")
+    # abrir aba
+    pyautogui.hotkey("ctrl", "t")
 
-# entrar no link do email - https://mail.google.com/mail/u/0/#inbox
-pyperclip.copy("https://mail.google.com/mail/u/0/#inbox")
-pyautogui.hotkey("ctrl", "v")
-pyautogui.press("enter")
-time.sleep(5)
+    # entrar no link do email - https://mail.google.com/mail/u/0/#inbox
+    pyperclip.copy("https://mail.google.com/mail/u/0/#inbox")
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.press("enter")
+    time.sleep(5)
 
-# clicar no botão escrever
-pyautogui.click(x=167, y=244)
+    # clicar no botão escrever
+    pyautogui.click(x=167, y=244)
 
-# preencher as informações do e-mail
-pyautogui.write("Coloque o Email Destinatário")
-pyautogui.press("tab") # selecionar o email
+    # preencher as informações do e-mail
+    pyautogui.write("Coloque o Email Destinatário")
+    pyautogui.press("tab") # selecionar o email
 
-pyautogui.press("tab") # pular para o campo de assunto
-pyperclip.copy("Relatório de Vendas")
-pyautogui.hotkey("ctrl", "v")
+    pyautogui.press("tab") # pular para o campo de assunto
+    pyperclip.copy("Relatório de Vendas")
+    pyautogui.hotkey("ctrl", "v")
 
-pyautogui.press("tab") # pular para o campo de corpo do email
+    pyautogui.press("tab") # pular para o campo de corpo do email
 
-texto = f"""
-Prezados,
+    texto = f"""
+    Prezados,
 
-Segue relatório de vendas.
-Faturamento: R${faturamento:,.2f}
-Quantidade de produtos vendidos: {quantidade:,}
+    Segue relatório de vendas.
+    Faturamento: R${faturamento:,.2f}
+    Quantidade de produtos vendidos: {quantidade:,}
 
-Qualquer dúvida estou à disposição.
-Att.,
-Lucas Ribeiro
-"""
+    Qualquer dúvida estou à disposição.
+    Att.,
+    Lucas Ribeiro
+    """
 
-# formatação dos números (moeda, dinheiro)
+    # formatação dos números (moeda, dinheiro)
 
-pyperclip.copy(texto)
-pyautogui.hotkey("ctrl", "v")
+    pyperclip.copy(texto)
+    pyautogui.hotkey("ctrl", "v")
 
-# enviar o e-mail
-pyautogui.hotkey("ctrl", "enter")
+    # enviar o e-mail
+    pyautogui.hotkey("ctrl", "enter")
+
+    #acabou
+    pyautogui.alert("Pronto, código executado com sucesso!")
+except Exception:
+    pyautogui.alert("Verifique seu código, algo deu errado na execução!\n"
+    "Dica: confira se você alterou o caminho do arquivo\n"
+    "Ou se você alterou as coordenadas de clique corretamente")
+
 
 
